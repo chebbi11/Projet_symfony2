7ca256ecd94824a9f61fba4314b2c20f0c5eef3f
Khaireddine Rhouma <92954403+Icyhell72@users.noreply.github.com>
2022-11-30 21:01:14 +0100

    Add files via upload

---
+++ migrations/Version20221127180009.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20221127180009 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE consultation (id INT AUTO_INCREMENT NOT NULL, fiche_id INT DEFAULT NULL, maladie VARCHAR(255) NOT NULL, allergie VARCHAR(255) NOT NULL, traitement VARCHAR(255) NOT NULL, date VARCHAR(255) NOT NULL, INDEX IDX_964685A6DF522508 (fiche_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE fiche (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(255) NOT NULL, prenom VARCHAR(255) NOT NULL, age INT NOT NULL, poids VARCHAR(255) NOT NULL, taille VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE consultation ADD CONSTRAINT FK_964685A6DF522508 FOREIGN KEY (fiche_id) REFERENCES fiche (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE consultation DROP FOREIGN KEY FK_964685A6DF522508');
+        $this->addSql('DROP TABLE consultation');
+        $this->addSql('DROP TABLE fiche');
+    }
+}
